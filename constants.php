b01b83c064458ba469329c24df358a09889efbc2
Pavel Kondratev <pablo.osamu@yandex.ru>
2018-06-03 20:27:30 +0400

    Delete constants.php

--- constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Константы базы данных
-    define("DB_SERVER", "m14.i.h.mchost.ru");
-    define("DB_USER", "a229419_1");
-    define("DB_PASS", "userbd");
-    define("DB_NAME", "a229419_1");
-?>
